827d740c525cef4d1128ed2661171c937accf80a
Katharine Xiao <kjx@mit.edu>
2015-01-09 15:43:40 -0500

    adding utils, passport, fixing some model code

--- gra/routes/index.js
+++ gra/routes/index.js
@@ -2,14 +2,18 @@ var express = require('express');
 var router = express.Router();
 var fs = require('fs-extra');
 
+var passport = require("passport");
+var LocalStrategy = require("passport-local").Strategy;
+var Client = require("../models/client").Client;
+
 /* GET home page. */
 router.get('/', function(req, res) {
   res.render('index', {});
 });
 
-/* GET login page. */
-router.get('/login', function(req, res) {
-	res.render('login', {});
+/* POST login user. */
+router.post("/login", passport.authenticate("local-login"), function(req, res) {
+    utils.sendSuccessResponse(res, {user: req.user});
 });
 
 router.post('/', function (req, res) {
@@ -34,6 +38,34 @@ router.post('/', function (req, res) {
             res.statusCode = 500;
             res.write("")
             res.end();
+        }
+    });
+});
+
+// Passport strategy for logging in
+passport.use("local-login", new LocalStrategy(function(username, password, done) {
+
+    Client.login(username, password, function(err, user) {
+        if (err) {
+            return done(err);
+        } else if (!user) {
+            return done(null, false, {message: "Invalid username/password"});
+        } else {
+            return done(null, user);
+        }
+    });
+}));
+
+passport.serializeUser(function(user, done) {
+    done(null, user.username);
+});
+
+passport.deserializeUser(function(username, done) {
+    Client.findByUsername(username, function(err, user) {
+        if (err) {
+            done(err);
+        } else {
+            done(null, user);
         }
     });
 });
---
+++ gra/utils/utils.js
@@ -0,0 +1,38 @@
+var utils = {};
+var validator = require('validator');
+
+// https://github.com/kongming92/6170-p3demo/blob/master/utils/utils.js
+/*
+  Send a 200 OK with success:true in the request body to the
+  response argument provided.
+  The caller of this function should return after calling
+*/
+utils.sendSuccessResponse = function(res, content) {
+  res.status(200).json({
+    success: true,
+    content: content
+  }).end();
+};
+
+/*
+  Send an error code with success:false and error message
+  as provided in the arguments to the response argument provided.
+  The caller of this function should return after calling
+*/
+utils.sendErrResponse = function(res, errcode, err) {
+  res.status(errcode).json({
+    success: false,
+    err: err
+  }).end();
+};
+
+
+utils.restrict = function(req, res, next) {
+    if (req.user) {
+        next();
+    } else {
+        utils.sendErrResponse(res, 401, 'Access denied!');
+    }
+}
+
+module.exports = utils;
