3abd9487e5d0ec5cd14574e45f2f0345afa43080
Jamar Brooks <j388923r@mit.edu>
2015-01-13 17:31:24 -0500

    moved things to gui.ejs from gui.html

---
+++ gra/public/javascripts/angularApp/guiApp.js
@@ -0,0 +1,31 @@
+﻿(function () {
+    'use strict';
+
+    var app = angular.module('guiApp', [
+        // Angular modules 
+        'ngAnimate',
+        'ngRoute',
+        'ngCookies',
+
+        // Custom modules 
+        'numbers'
+        // 3rd Party Modules
+
+    ]);
+
+    angular.module('numbers', []).filter('maxHundred', function () {
+        return function (input) {
+            return Math.min(input, 100);
+        };
+    });
+
+    app.config(['$routeProvider',
+        function ($routeProvider) {
+            $routeProvider.
+                when('/', {
+                    templateUrl: '/angularviews/gui.html',
+                    controller: 'guiController'
+                });
+        }
+    ])
+})();
